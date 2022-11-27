f8c743dd61905f69d1e3e09757ff86791fec970f
hailthedawn <kkt7778@rit.edu>
2022-11-27 16:23:43 -0500

    logistic regression working. cleaning up code now

--- logistic_regression.py
+++ logistic_regression.py
@@ -13,7 +13,6 @@ class LogisticRegression():
     def gradients(self, X, y, y_hat):
     
         n = X.shape[0]
-
         dw = (1/n)*Utils.mxmult(Utils.transpose(self.X_train), (y_hat - y))
         db = (1/n)*np.sum((y_hat - y)) 
         
@@ -24,12 +23,15 @@ class LogisticRegression():
         return 1.0/(1 + np.exp(-z))
 
     def h(self, x, beta, bias):
-        print(x.shape, beta.shape, "eee", Utils.mxmult(x, beta).shape)
+        # print("aa", x[0])
+        # print((Utils.mxmult(x, beta)))
         return self.sigmoid(Utils.mxmult(x, beta) + bias)
 
-    def cost(self, weights):
-        n_rows =  (2*self.y_train.shape[0])
-        h_x = self.h(weights)
+    def get_cost(self, weights, bias):
+        n_rows =  (self.y_train.shape[0])
+        # print(self.X_train.shape, weights.shape, bias,"aeee")
+        h_x = self.h(self.X_train, weights, bias)
+        # print("hx", (self.X_train))
         cost = -(self.y_train*(np.log(h_x)) - (1-self.y_train)*np.log(1-h_x))/n_rows
         return cost
 
@@ -37,10 +39,12 @@ class LogisticRegression():
         accuracy = np.sum(y == y_hat) / len(y)
         return accuracy
 
-    def fit(self, lr=0.1, epochs=10, batchsize=20):
+    def fit(self, lr=0.1, epochs=10, batchsize=100):
         n = self.X_train.shape[0]
         w = np.zeros((self.X_train.shape[1], 1))
-        b =  np.zeros((batchsize, 1))
+       
+        b =  0
+        losses = []
 
         for epoch in range(0,epochs):
             for i in range((n-1)//batchsize + 1):
@@ -48,16 +52,33 @@ class LogisticRegression():
                 last_idx = first_idx + batchsize
                 xb, yb = self.X_train[first_idx:last_idx], self.y_train[first_idx:last_idx]
                 y_hat = self.h(xb, w, b)
-                dw, db = self.gradients(xb, yb, y_hat)
-
-                w = w - lr*dw
-                b = b - lr*dw
+                dw, db = self.gradients(xb, yb, y_hat) # yhat is nan
+                w = w - lr*dw   
+                b = b - lr*db
+                
             
-            l = get_cost(self.y_train, self.h(self.X_train, w, b))
+            l = self.get_cost(w, b)
             losses.append(l)
+            break
 
-        print("Losses: ", losses)
+        # print("Losses: ", losses)
         return w, b, losses
+    
+    def predict(self, w, b):
+        X = self.X_test
+        preds = self.h(X, w, b)
+        
+        # Empty List to store predictions.
+        pred_class = []
+        # if y_hat >= 0.5 --> round up to 1
+        # if y_hat < 0.5 --> round up to 1
+        pred_class = [1 if i > 0.5 else 0 for i in preds]
+        
+        return np.array(pred_class)
+
+    def accuracy(self, predicted):
+        accuracy = np.sum(self.y_test[0] == predicted) / len(self.y_test)
+        return accuracy
 
 
 def handle_nulls(df):
@@ -70,10 +91,11 @@ if __name__ == "__main__":
     # Data source: https://www.kaggle.com/code/dyasin/week24ml-weather-dataset-rattle-package-weatheraus/data 
     
     df = pd.read_csv("weatherAUS.csv")
+    df = df.iloc[:10]
     y = pd.get_dummies(df.RainTomorrow, drop_first=True)
     y = y.values.reshape(-1,1)
     # df = encode_categorical_vars(df)
-    df.drop(['Date', 'Location', 'WindGustDir', 'WindDir9am', 'WindDir3pm', 'RainToday',  "RainTomorrow"],  axis=1, inplace=True)
+    df.drop(['Date', 'Location', 'WindGustDir', 'WindDir9am', 'Evaporation', 'Sunshine', 'WindDir3pm', 'RainToday',  "RainTomorrow"],  axis=1, inplace=True)
     handle_nulls(df)
 
     # Normalize Data
@@ -86,5 +108,7 @@ if __name__ == "__main__":
    
     logregmodel = LogisticRegression(X_train, X_test, y_train, y_test)
 
-    J, beta = logregmodel.fit(lr=0.01, epochs=400)
-    # linregmodel.predict(beta)
+    weights, bias, losses = logregmodel.fit(lr=0.01, epochs=10)
+    y_pred = logregmodel.predict(weights, bias)
+    
+    print("Accuracy: ",logregmodel.accuracy(y_pred))
