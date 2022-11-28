2ddf7c69053616b62f1fae968e92820106c6fecb
hailthedawn <kkt7778@rit.edu>
2022-11-27 21:25:50 -0500

    sklearn model added to log reg

--- test_logistic_reg.py
+++ test_logistic_reg.py
@@ -1,6 +1,9 @@
 import numpy as np
 import pandas as pd
 from statsbox.logistic_regression import LogisticRegression
+from sklearn.linear_model import LogisticRegression as sklearn_lr
+
+from sklearn.metrics import accuracy_score
 
 # Logistic Regression Code Reference: https://towardsdatascience.com/logistic-regression-from-scratch-in-python-ec66603592e2
 # Data source: https://www.kaggle.com/code/dyasin/week24ml-weather-dataset-rattle-package-weatheraus/data 
@@ -9,6 +12,12 @@ def handle_nulls(df):
     for col in df:
         col_median=df[col].median()
         df[col].fillna(col_median, inplace=True)
+
+def run_sklearn_model():
+    sklearn_model = sklearn_lr(solver='liblinear', random_state=0)
+    sklearn_model.fit(X_train, y_train)
+    y_pred_test = sklearn_model.predict(X_test)
+    print('Sklearn model accuracy score: {0:0.4f}'. format(accuracy_score(y_test, y_pred_test)))
 
 df = pd.read_csv("weatherAUS.csv")
 y = pd.get_dummies(df.RainTomorrow, drop_first=True)
@@ -28,7 +37,9 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=.2, random_s
 
 logregmodel = LogisticRegression(X_train, X_test, y_train, y_test)
 
-weights, bias, losses = logregmodel.fit(lr=0.01, epochs=10, batchsize=10000)
+weights, bias, losses = logregmodel.fit(lr=0.01, epochs=100, batchsize=1000)
 y_pred = logregmodel.predict(weights, bias)
 
 print("Accuracy: ",logregmodel.accuracy(y_pred))
+
+run_sklearn_model()
